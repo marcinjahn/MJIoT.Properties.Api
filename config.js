723eb5eb06e5a27395de89e8cdc5baaf74825a5a
Marcin Jahn <jahnmarcin@outlook.com>
2018-07-16 22:35:30 +0200

    Remove unnecessary files

--- config.js
+++
@@ -1,8 +0,0 @@
-let config = {}
-
-config.host = "https://mjiot.documents.azure.com:443/";
-config.authKey = "RzqbIUJO0qFiWSl6gMXvfmAeGZweUCpdK0OL9cP7aWolyWxvBaukRZTayWBdsqZl6kAfqbF4OR8kmfvjsN4mCg==";
-config.databaseId = "MJIoTCosmosDB";
-config.collectionId = "DevicesPropertiesData";
-
-module.exports = config;
